0b4c14b153acde94c2ca31c3444b31d5bd95243a
LukasGlahn <Lukas.glahn@gmail.com>
2023-10-03 16:40:35 +0200

    Update Id_3.py
    pb added

--- Id_3.py
+++ Id_3.py
@@ -1,7 +1,7 @@
 from machine import Pin, ADC
 from neopixel import NeoPixel
 from machine import Pin
-from time import sleep_ms
+from time import sleep_ms, sleep
 
 PB1_PIN = 4
 pb1 = Pin(PB1_PIN, Pin.IN)
@@ -12,6 +12,7 @@ n = 12 #number of pixels
 p = 26 #pin acchachet to ring 
 np = NeoPixel(Pin(p, Pin.OUT), n)
 light_nr = -1
+pb1_pushed = 1
 
 def led_step(r,g,b):
     global light_nr
@@ -24,7 +25,21 @@ def led_step(r,g,b):
     np[light_nr] = (r,g,b)
     np.write()
 
+
 while True:
-    pot_val = pot.read()
-    led_step(80,0,0)
-    sleep_ms(pot_val)
+    first = pb1.value()
+    sleep(0.05)
+    second = pb1.value()
+    
+    if first > second:
+        while True:
+            pot_val = pot.read()
+            led_step(80,0,0)
+            #sov i den tid som potentiometerts værdi er
+            first = pb1.value()
+            sleep_ms(pot_val)
+            second = pb1.value()
+            #stop koden vis knapen er trykket
+            if first > second:
+                break
+  
